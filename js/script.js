10cafe8199461a153a2d984d0437e8c788d31eba
Emmanuel Nartey <emmanuelnartey@Emmanuels-MacBook-Pro.local>
2018-10-25 21:59:30 +0000

    First commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const API_publicKey = "FLWPUBK-24b72aebb821aea177483039677df9d3-X";
+
+    function payWithRave() {
+        var x = getpaidSetup({
+            PBFPubKey: API_publicKey,
+            customer_email: "user@example.com",
+            amount: 2000,
+            customer_phone: "234099940409",
+            currency: "NGN",
+            payment_options: "card",
+            txref: "rave-123456",
+            meta: [{
+                metaname: "flightID",
+                metavalue: "AP1234"
+            }],
+            onclose: function() {},
+            callback: function(response) {
+                var txref = response.tx.txRef; // collect flwRef returned and pass to a 					server page to complete status check.
+                console.log("This is the response returned after a charge", response);
+                if (
+                    response.tx.chargeResponseCode == "00" ||
+                    response.tx.chargeResponseCode == "0"
+                ) {
+                    // redirect to a success page
+                } else {
+                    // redirect to a failure page.
+                }
+
+                x.close(); // use this to close the modal immediately after payment.
+            }
+        });
+    }
